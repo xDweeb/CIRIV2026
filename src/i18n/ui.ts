828acd42859a1d5e2267e042b4b75925025e3b5a
xdweb <xdweb.elyakouti@gmail.com>
2025-12-10 18:27:35 +0100

    fix remarque sur site

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -19,7 +19,7 @@ export const ui = {
     'cta.inscription': 'Inscription',
     'cta.register': 'S\'inscrire maintenant',
     'hero.title': 'Congrès International de la Recherche, d\'Innovation et Valorisation',
-    'hero.subtitle': '1er édition : One Health',
+    'hero.subtitle': '1re édition : One Health',
     'hero.trustedBy': 'Soutenu par',
     'footer.quickLinks': 'Liens rapides',
     'footer.copyright': '© 2026 CIRIV. Tous droits réservés.',
